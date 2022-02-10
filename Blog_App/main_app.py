4df5e8f9f2668da9ee26b83f911932330a599e6d
yesandrei <80432094+yesandrei@users.noreply.github.com>
2022-02-10 15:01:04 +1300

    Update main_app.py
    Changed Hello World to "Home Page" and added an about page so there is now two pages on the web app

--- Blog_App/main_app.py
+++ Blog_App/main_app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<h1>Hello, World!</h1>"
+    return "<h1>Home Page</h1>"
+
+@app.route("/about")
+def about():
+    return "<h1>About PAge</h1>"
 
 
 if __name__=='__main__':
