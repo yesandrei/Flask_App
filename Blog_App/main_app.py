2c3e575beb1519961c21b2e46d88ca3d08a9c97c
yesandrei <80432094+yesandrei@users.noreply.github.com>
2022-02-15 12:29:33 +1300

    (Not Working)Added posts, lists and python in the html file
    (Not Working) Added posts, lists and python in the html file

--- Blog_App/main_app.py
+++ Blog_App/main_app.py
@@ -1,13 +1,34 @@
+from pdb import post_mortem
 from flask import Flask, render_template
-
 app = Flask(__name__)
+
+#flask also makes python able to put python programming inside an html document
+
+posts = [
+    
+    {
+        'author':'Andrei M',
+        'title':'Blog Post 1',
+        'content':'First Post Content',     
+        'date_posted':'Febuary 2 2022'
+    },
+    
+    {
+        'author':'Jane Doe',
+        'title':'Blog Post 2',
+        'content':'Second Post Content',     
+        'date_posted':'Febuary 4 2022'
+    },
+    
+]
 
 #The below code links everything to the other html files in the folder.
 #If theres nothing when it links it will link to the home page.
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html')
+#post=posts puts the posts variable into the html file so we can use it
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
