f5ac21e15e9c82b6d00434960984cf66f84c265c
yesandrei <80432094+yesandrei@users.noreply.github.com>
2022-02-21 11:57:34 +1300

    Created a forms file
    Made a Registration and Login Form

---
+++ Blog_App/forms.py
@@ -0,0 +1,23 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+#wtf stands for wtforms
+#Data required means not_blank
+#Above is just importing validators from the pip3 install-wtf in the cmd prompt
+
+#Makes FlaskForm avaliable to the class
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()] )
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+#submit is a button not a text input
+#The red brackets is what will be seen - the label
+
+class RegistrationForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()] )
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember me')
+    submit = SubmitField('Login')
+#Boeolean Field is yes or no?
