e74cfeeaf4da84687966001c50d3ca62fd0dd712
yesandrei <80432094+yesandrei@users.noreply.github.com>
2022-06-08 14:59:01 +1200

    Lesson 22 Added Validation
    Added Validation for username and email so they are unique when registering

--- Blog_App/main_app/forms.py
+++ Blog_App/main_app/forms.py
@@ -1,6 +1,8 @@
+from xml.dom import ValidationErr
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from main_app.models import User
 #wtf stands for wtforms
 #Data required means not_blank
 #Above is just importing validators from the pip3 install-wtf in the cmd prompt
@@ -15,6 +17,16 @@ class RegistrationForm(FlaskForm):
 #submit is a button not a text input
 #The red brackets is what will be seen - the label
 
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError("That username is taken. Please choose another one")
+        
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError("That email is taken. Please choose another one")
+    
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()] )
     password = PasswordField('Password', validators=[DataRequired()])
