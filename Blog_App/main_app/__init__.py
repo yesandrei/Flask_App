923b49b8cc0d1282d44da47c3746abe18ff58d62
yesandrei <80432094+yesandrei@users.noreply.github.com>
2022-07-25 12:24:43 +1200

    Redirect User when unauthenticated  use

--- Blog_App/main_app/__init__.py
+++ Blog_App/main_app/__init__.py
@@ -12,5 +12,9 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from main_app import routes
+
+#login view makes it so that u need to be logged into an account to access the account page. If not logged in it will send the user to the login page. The 'info' is bootstrap which makes it blue.
